9c2006a7d326c1d6901da66f94534294d535dbc9
Kyle Amoroso <kamoroso1994@gmail.com>
2019-04-05 21:13:59 -0400

    Change spread behavior to accommodate new salt feature

--- Cells.js
+++ Cells.js
@@ -1,6 +1,5 @@
-// TODO:
-//  add more cell types
-//  hasGravity for air is weird
+// TODO: add more cell types, hasGravity for air is weird
+
 export const SIZE = 4;
 export const SPREAD_RATE = 1/16;
 
@@ -18,46 +17,76 @@ export const data = {
   "water": {
     "color": "#2020fe",
     "conversions": {
-      "sand-spout": "sand"
+      "sand-spout": {
+        "other": "sand"
+      }
     },
     "density": 2,
     "hasGravity": true
   },
+  "saltwater": {
+    "color": "#4080ff",
+    "density": 3,
+    "hasGravity": true
+  },
+  "salt": {
+    "color": "#ffffff",
+    "conversions": {
+      "water": {
+        "self": "air",
+        "other": "saltwater"
+      }
+    },
+    "density": 4,
+    "hasGravity": true
+  },
   "sand": {
     "conversions": {
-      "oil-spout": "sand",
-      "water-spout": "sand"
+      "oil-spout": {
+        "other": "sand"
+      },
+      "water-spout": {
+        "other": "sand"
+      }
     },
     "color": "#eecc80",
-    "density": 3,
+    "density": 4,
     "hasGravity": true
   },
   "ground": {
     "color": "#aa8820"
   },
   "plant": {
+    "color": "#20cc20",
     "conversions": {
-      "water": "plant"
-    },
-    "color": "#20cc20"
+      "water": {
+        "other": "plant"
+      }
+    }
   },
   "oil-spout": {
+    "color": "#cc6666",
     "conversions": {
-      "air": "oil"
-    },
-    "color": "#cc6666"
+      "air": {
+        "other": "oil"
+      }
+    }
   },
   "water-spout": {
     "conversions": {
-      "air": "water"
+      "air": {
+        "other": "water"
+      }
     },
     "color": "#70a0ff"
   },
   "sand-spout": {
+    "color": "#edb744",
     "conversions": {
-      "air": "sand"
-    },
-    "color": "#edb744"
+      "air": {
+        "other": "sand"
+      }
+    }
   }
 };
 
@@ -72,11 +101,14 @@ export function convert(self, other) {
   if(self == null || other == null) return false;
 
   const selfData = data[self.id];
-  const otherData = data[other.id];
   if(!selfData.hasOwnProperty('conversions')) return false;
   if(!selfData.conversions.hasOwnProperty(other.id)) return false;
 
-  other.id = selfData.conversions[other.id];
+  const selfId = selfData.conversions[other.id].self;
+  const otherId = selfData.conversions[other.id].other;
+  if(selfId != null) self.id = selfId;
+  if(otherId != null) other.id = otherId;
+
   return true;
 }
 
